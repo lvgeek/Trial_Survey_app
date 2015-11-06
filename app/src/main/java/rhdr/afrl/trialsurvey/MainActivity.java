4c3592ee55feb6270cd3d5e52fa7990854c7aff2
dave <lvgeek101@gmail.com>
2015-11-06 14:17:11 -0600

    Initial commit

---
+++ app/src/main/java/rhdr/afrl/trialsurvey/MainActivity.java
@@ -0,0 +1,14 @@
+package rhdr.afrl.trialsurvey;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
