247012a2a19aa5709a1721a8a5ca5322524034c7
dave <lvgeek101@gmail.com>
2015-11-06 14:53:52 -0600

    Added NewProtocol Activity

--- app/src/main/java/rhdr/afrl/trialsurvey/MainActivity.java
+++ app/src/main/java/rhdr/afrl/trialsurvey/MainActivity.java
@@ -2,6 +2,8 @@ package rhdr.afrl.trialsurvey;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    /** Called when the user clicks the New Protocal button */
+    public void showNewProto(View view) {
+        Intent intent = new Intent(this, DisplayNewProtocolActivity.class);
+        startActivity(intent);
     }
 
 }
