dd5340ed9f92eee39ec6ac1d4728a557a34ef1b5
dave <lvgeek101@gmail.com>
2015-11-12 11:09:16 -0600

    DisplayNewProtocol and DisplayEditProtocol code changed

--- app/src/main/java/rhdr/afrl/trialsurvey/DisplayEditProtocolActivity.java
+++ app/src/main/java/rhdr/afrl/trialsurvey/DisplayEditProtocolActivity.java
@@ -2,8 +2,13 @@ package rhdr.afrl.trialsurvey;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class DisplayEditProtocolActivity extends AppCompatActivity {
+    EditText txtProtocolID;
+    EditText txtNumSubjects;
+    EditText txtNumShotcode;
+    DBHandler db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
