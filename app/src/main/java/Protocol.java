b8ba5f3c0f7d0532d94e3a793ae24429d59c5587
dave <lvgeek101@gmail.com>
2015-11-12 07:08:18 -0600

    added DBHandler and Protocol classes

---
+++ app/src/main/java/Protocol.java
@@ -0,0 +1,71 @@
+/**
+ * Created by dave on 11/11/15.
+ */
+package rhdr.afrl.trialsurvey;
+
+public class Protocol {
+
+    //private variables
+    int _id;
+    String _name;
+    String _numSubjects;
+    String _numShotcodes;
+
+    // Empty constructor
+    public Protocol(){
+
+    }
+    // constructor
+    public Protocol(int id, String name, String _numSubjects, String _numShotcodes){
+        this._id = id;
+        this._name = name;
+        this._numSubjects = _numSubjects;
+        this._numShotcodes =  _numShotcodes;
+    }
+
+    // constructor
+    public Protocol(String name, String _numSubjects, String _numShotcodes){
+        this._name = name;
+        this._numSubjects = _numSubjects;
+        this._numShotcodes =  _numShotcodes;
+    }
+    // getting ID
+    public int getID(){
+        return this._id;
+    }
+
+    // setting id
+    public void setID(int id){
+        this._id = id;
+    }
+
+    // getting name
+    public String getName(){
+        return this._name;
+    }
+
+    // setting name
+    public void setName(String name){
+        this._name = name;
+    }
+
+    // getting numSubjects
+    public String getnumSubjects(){
+        return this._numSubjects;
+    }
+
+    // setting numSubjects
+    public void setPhoneNumber(String numSubjects){
+        this._numSubjects = numSubjects;
+    }
+
+    // getting numShotcodes
+    public String getnumShotcodes(){
+        return this._numSubjects;
+    }
+
+    // setting numShotcodes
+    public void setnumShotcodes(String numShotcodes){
+        this._numShotcodes = numShotcodes;
+    }
+}
