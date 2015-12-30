7ee0082a037933c977c4be05e99639602980d677
dave <lvgeek101@gmail.com>
2015-12-30 15:59:07 -0600

    added medical monitor

--- app/src/main/java/rhdr/afrl/trialsurvey/RunProtocolActivity.java
+++ app/src/main/java/rhdr/afrl/trialsurvey/RunProtocolActivity.java
@@ -16,9 +16,11 @@ public class RunProtocolActivity extends AppCompatActivity implements AdapterVie
 
     // define elements
     String[] protocols;
+    String[] medMonitors;
     String[] subjects;
     String[] shotcodes;
     Spinner spnrProtocolID;
+    Spinner spnrmedMonitorID;
     Spinner spnrSubjectID;
     Spinner spnrShotCodeID;
     Button btn;
@@ -38,11 +40,13 @@ public class RunProtocolActivity extends AppCompatActivity implements AdapterVie
         btn.setEnabled(false);
 
         protocols = getResources().getStringArray(R.array.protocol_list);
+        medMonitors = getResources().getStringArray(R.array.medMonitor_list);
         subjects = getResources().getStringArray(R.array.subject_list);
         shotcodes = getResources().getStringArray(R.array.shotcode_list);
 
         // link elements
         spnrProtocolID = (Spinner) findViewById(R.id.spnrProtocolID);
+        spnrmedMonitorID = (Spinner) findViewById(R.id.spnrmedMonitorID);
         spnrSubjectID = (Spinner) findViewById(R.id.spnrSubjectID);
         spnrShotCodeID = (Spinner) findViewById(R.id.spnrShotCodeID);
 
@@ -51,20 +55,27 @@ public class RunProtocolActivity extends AppCompatActivity implements AdapterVie
         dataAdapter.setDropDownViewResource(R.layout.spinner_item);
         spnrProtocolID.setAdapter(dataAdapter);
 
+        ArrayAdapter<String> dataAdaptermed = new ArrayAdapter<String>(this,
+                R.layout.spinner_item, medMonitors);
+        dataAdaptermed.setDropDownViewResource(R.layout.spinner_item);
+        spnrmedMonitorID.setAdapter(dataAdaptermed);
+
         ArrayAdapter<String> dataAdaptersubject = new ArrayAdapter<String>(this,
                 R.layout.spinner_item, subjects);
-        dataAdapter.setDropDownViewResource(R.layout.spinner_item);
+        dataAdaptersubject.setDropDownViewResource(R.layout.spinner_item);
         spnrSubjectID.setAdapter(dataAdaptersubject);
 
         ArrayAdapter<String> dataAdaptershotcode = new ArrayAdapter<String>(this,
                 R.layout.spinner_item, shotcodes);
-        dataAdapter.setDropDownViewResource(R.layout.spinner_item);
+        dataAdaptershotcode.setDropDownViewResource(R.layout.spinner_item);
         spnrShotCodeID.setAdapter(dataAdaptershotcode);
 
+        spnrmedMonitorID.setSelection(0);
         spnrSubjectID.setSelection(0);
         spnrShotCodeID.setSelection(0);
 
         // Spinner click listener
+        spnrmedMonitorID.setOnItemSelectedListener(this);
         spnrSubjectID.setOnItemSelectedListener(this);
         spnrShotCodeID.setOnItemSelectedListener(this);
 
@@ -74,7 +85,7 @@ public class RunProtocolActivity extends AppCompatActivity implements AdapterVie
     @Override
     public void onItemSelected(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
 
-        if (spnrSubjectID.getSelectedItemPosition()!=0 && spnrShotCodeID.getSelectedItemPosition()!=0) {
+        if (spnrSubjectID.getSelectedItemPosition()!=0 && spnrShotCodeID.getSelectedItemPosition()!=0 && spnrmedMonitorID.getSelectedItemPosition()!=0) {
             btn.setEnabled(true);
         }else {
             btn.setEnabled(false);
@@ -95,6 +106,8 @@ public class RunProtocolActivity extends AppCompatActivity implements AdapterVie
         // read all spinner values to pass to RunTrialActivity
         final Spinner protocol = (Spinner) findViewById((R.id.spnrProtocolID));
         final String protocolVal = String.valueOf(protocol.getSelectedItem());
+        final Spinner medMonitor = (Spinner) findViewById((R.id.spnrmedMonitorID));
+        final String medMonitorVal = String.valueOf(medMonitor.getSelectedItem());
         final Spinner subject = (Spinner) findViewById((R.id.spnrSubjectID));
         final String subjectVal = String.valueOf(subject.getSelectedItem());
         final Spinner shotcode = (Spinner) findViewById((R.id.spnrShotCodeID));
@@ -102,8 +115,11 @@ public class RunProtocolActivity extends AppCompatActivity implements AdapterVie
 
         Intent intent = new Intent(this, RunTrialActivity.class);
         intent.putExtra("Protocol", protocolVal);
+        intent.putExtra("MedMonitor", medMonitorVal);
         intent.putExtra("Subject",subjectVal);
         intent.putExtra("Shotcode",shotcodeVal);
         startActivity(intent);
+        spnrSubjectID.setSelection(0);
+        spnrShotCodeID.setSelection(0);
     }
 }
